38e8b5a75dff1367ac18587cf98b1c7b4cc7c15c
Sammi De Guzman <sammi@sammdot.ca>
2021-01-03 15:02:25 -0800

    Switch to Gemini PR protocol

--- keyboards/splitography/keymaps/sammdot/keymap.c
+++ keyboards/splitography/keymaps/sammdot/keymap.c
@@ -24,8 +24,8 @@
 extern keymap_config_t keymap_config;
 
 enum splitography_layers {
-  _TXBOLT = 0
- ,_END_LAYERS
+  _GEMINI = 0,
+  _END_LAYERS
 };
 
 #define ___x___ KC_TRNS
@@ -36,7 +36,7 @@ enum splitography_layers {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-  [_TXBOLT] = {
+  [_GEMINI] = {
     {STN_N1,  STN_N2,  STN_N3,  STN_N4,  STN_N5,  STN_N6,  STN_N7,  STN_N8,  STN_N9,  STN_NA,  STN_NB,  STN_NC },
     {_______, STN_S1,  STN_TL,  STN_PL,  STN_HL,  STN_ST1, STN_ST3, STN_FR,  STN_PR,  STN_LR,  STN_TR,  STN_DR },
     {_______, STN_S2,  STN_KL,  STN_WL,  STN_RL,  STN_ST2, STN_ST4, STN_RR,  STN_BR,  STN_GR,  STN_SR,  STN_ZR },
@@ -47,5 +47,5 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 void matrix_init_user(void)
 {
-  steno_set_mode(STENO_MODE_BOLT);
+  steno_set_mode(STENO_MODE_GEMINI);
 }
